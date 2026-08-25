269b053d9b90714a5fa4682bffecf7ec738809d9
Nufail Shaikh <nufailshaikh786@gmail.com>
2026-08-25 07:28:37 +0530

    Update certifications.js

--- src/constants/certifications/certifications.js
+++ src/constants/certifications/certifications.js
@@ -41,12 +41,12 @@ export const CERTIFICATIONS = [
     image: "/certifications/treasure-hunt.jpg",
     credentialUrl: "#",
   },
-  {
-    id: "completion-of-internship",
-    title: "Completion of Internship",
-    image: "/certifications/completion-of-internship.jpg",
-    credentialUrl: "#",
-  },
+  // {
+  //   id: "completion-of-internship",
+  //   title: "Completion of Internship",
+  //   image: "/certifications/completion-of-internship.jpg",
+  //   credentialUrl: "#",
+  // },
   {
     id: "pronttera-certificate",
     title: "Pronttera Certificate",
